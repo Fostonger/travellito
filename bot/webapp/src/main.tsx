e3c1c5d7655d2553833219f44cb1767637dd419b
Bulat Musin <66408714+Fostonger@users.noreply.github.com>
2025-07-20 00:36:27 +0300

    Remove React Query Devtools from main application entry point to streamline the build and reduce unnecessary dependencies.

--- bot/webapp/src/main.tsx
+++ bot/webapp/src/main.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './pages/App';
 import TourDetail from './pages/TourDetail';
 import Checkout from './pages/Checkout';
@@ -65,7 +64,6 @@ root.render(
           <Route path="/bookings" element={<MyBookings />} />
         </Routes>
       </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 ); 
